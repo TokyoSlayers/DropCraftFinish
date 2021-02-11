4268d12570e5f325337d859ec1559a13a3e0052d
Asus-tuf <therualaz.max@hotmail.fr>
2021-02-11 11:53:07 +0100

    finish

---
+++ src/main/java/net/DropCraft/Finish/Event/MySQLReloadEvent.java
@@ -0,0 +1,41 @@
+package net.DropCraft.Finish.Event;
+
+import net.DropCraft.Finish.Utils.MySQL;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.jetbrains.annotations.NotNull;
+
+public class MySQLReloadEvent extends Event {
+
+    private int counter;
+
+    private static final HandlerList HANDLERS_LIST = new HandlerList();
+
+    public MySQLReloadEvent(int iReload) {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                counter++;
+
+                if (counter == iReload) {
+                    if (!MySQL.isConnected()) {
+                        MySQL.disconnect();
+                        MySQL.connect("91.234.195.40", "c1500257c_dropcraft-serv", 3306, "c1500257c_plugin", ";HPyt@fVBT47");
+                        System.out.println("MySQL ckeck REDEMARER !");
+                    } else {
+                        System.out.println("MySQL check OK !");
+                    }
+                    counter = 0;
+                }
+            }
+        };
+    }
+
+    @NotNull
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLERS_LIST;
+    }
+}
---
+++ src/main/java/net/DropCraft/Finish/Event/MySQLSendEvent.java
@@ -0,0 +1,80 @@
+package net.DropCraft.Finish.Event;
+
+import net.DropCraft.Finish.Utils.MySQL;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class MySQLSendEvent extends Event {
+
+
+    private static final HandlerList HANDLERS_LIST = new HandlerList();
+
+    public MySQLSendEvent(String state, String serverName) {
+        createTable();
+        if(getStatement(serverName).isEmpty()){
+            createStatement(state,serverName);
+        }else{
+            setStatement(state,serverName);
+        }
+    }
+
+    public void createTable(){
+        try {
+            PreparedStatement ps = MySQL.getConnection().prepareStatement("CREATE TABLE IF NOT EXISTS `DropCraftServ_State` (ServerName VARCHAR(255),Stat VARCHAR(255))");
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void createStatement(String state, String serverName){
+        try {
+            PreparedStatement sts = MySQL.getConnection().prepareStatement("SELECT `Stat` FROM `DropCraftServ_State` WHERE `ServerName`='" + serverName + "'");
+            ResultSet rs = sts.executeQuery();
+            if(!rs.next()) {
+                sts.close();
+                sts = MySQL.getConnection().prepareStatement("INSERT INTO `DropCraftServ_State` (ServerName, Stat) VALUES ('" + serverName + "','" + state + "')");
+                sts.executeUpdate();
+                sts.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setStatement(String state, String serverName){
+        try {
+            PreparedStatement sts = MySQL.getConnection().prepareStatement("UPDATE `DropCraftServ_State` SET `Stat`='" + state + "' WHERE `ServerName`='"+ serverName +"'");
+            sts.executeUpdate();
+            sts.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getStatement(String serverName){
+        String dif = "null";
+        try {
+            PreparedStatement sts = MySQL.getConnection().prepareStatement("SELECT `Stat` FROM `DropCraftServ_State` WHERE `ServerName`='" + serverName + "'");
+            ResultSet rs = sts.executeQuery();
+            if (rs.next()){
+                dif = rs.getString("Stat");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return dif;
+    }
+
+    @NotNull
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLERS_LIST;
+    }
+}
